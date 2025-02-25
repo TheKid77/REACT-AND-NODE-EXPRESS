4daee201d27eaa54d603b3d0355fdb979fccf5f9
TheKid77 <andymcc77@blueyonder.co.uk>
2025-02-25 09:06:31 +0000

    Add Menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   const [message, setMessage] = useState("");
   const [message2, setMessage2] = useState("");
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
 
   useEffect(() => {
     // Fetch data from the API endpoint
@@ -19,17 +34,43 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>React + Express App</h1>
-      TEST
+    <>
       <AppBar className={"appbar"}>
         <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography>{message}</Typography>
         </Toolbar>
       </AppBar>
-      <p>{message}</p>
-      <p>{message2}</p>
-    </div>
+
+      <Drawer open={isDrawerOpen} onClose={toggleDrawer}>
+        <List sx={{ width: 250 }} role="presentation" onClick={toggleDrawer}>
+          <ListItemButton>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemText primary="About" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemText primary="Contact" />
+          </ListItemButton>
+        </List>
+      </Drawer>
+
+      <div>
+        <h1>React + Express App</h1>
+        TEST
+        <AppBar className={"appbar"}></AppBar>
+        <p>{message}</p>
+        <p>{message2}</p>
+      </div>
+    </>
   );
 }
 
